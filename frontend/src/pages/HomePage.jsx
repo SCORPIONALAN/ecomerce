81ee0c4ec15a60f791b77e1aaf5caedd5c774442
SCORPIONALAN <alantm1103@gmail.com>
2025-03-04 21:07:51 -0600

    Boton funcional para borrar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,46 @@
+import {Container, VStack, Text} from '@chakra-ui/react';
+import { SimpleGrid } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useProductStore } from '../store/product';
+import ProductCard from '../components/ProductCard';
 const HomePage = () =>{
+    const {fetchProducts, products} = useProductStore()
+    useEffect(()=>{fetchProducts();},[fetchProducts])
+    console.log(products);
     return(
-        <div>HomePage</div>
+        <Container maxW={'container.xl'} py={12}>
+            <VStack spacing={8}>
+                <Text
+                    bgGradient={'linear(to-r,rgb(59, 198, 223),rgb(0, 17, 255))'}
+                    bgClip={'text'}
+                    fontSize={{base:"22", sm:"28"}}
+                    fontWeight={'bold'}
+                    textTransform={'uppercase'}
+                    textAlign={'center'}
+                    >
+                        Current Proyects 🚀
+                </Text>
+                {products.length > 0 && <SimpleGrid
+                    columns={{base:1, md:2, lg:3}}
+                    spacing={10}
+                    w={"full"}>
+                        {products.map((product) =>(
+                            <ProductCard key={product._id} product={product}/>
+                        ))}
+                </SimpleGrid>}
+                
+                {products.length === 0 &&
+                <Text fontSize={'xl'} textAlign={'center'} fontWeight={'bold'} color={'gray.500'}>
+                    No products found 😞 {" "}
+                    <Link to={"/create"}>
+                        <Text as='span' color='blue.500' _hover={{textDecoration:"underline"}}>
+                            Create a product
+                        </Text>
+                    </Link>
+                </Text>}
+            </VStack>
+        </Container>
     );
 }
 export default HomePage;
